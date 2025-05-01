958c08671391c690b35fc68f3e9c331a4c0e7220
J.Chris Findlay <j.chris.findlay@gmail.com>
2025-05-01 14:08:38 +1200

    :lipstick: Apply formatting.

--- datacomTest.Api/Program.cs
+++ datacomTest.Api/Program.cs
@@ -1,3 +1,5 @@
+using DatacomTest.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,35 +9,33 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
+if (app.Environment.IsDevelopment()) {
+	app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+var summaries = new[] {
+	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+app.MapGet("/weatherforecast", () => {
+	var forecast = Enumerable.Range(1, 5)
+		.Select(index =>
+			new WeatherForecast(
+				DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+				Random.Shared.Next(-20, 55),
+				summaries[Random.Shared.Next(summaries.Length)]
+			)
+		)
+		.ToArray();
+	return forecast;
 })
-.WithName("GetWeatherForecast");
+	.WithName("GetWeatherForecast");
+
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
+	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
